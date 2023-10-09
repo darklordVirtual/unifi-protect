2a6c66d1969c6e0557adb4304398278c81ed0364
Stian Skogbrott <36493828+darklordVirtual@users.noreply.github.com>
2023-10-09 10:20:30 +0200

    Create index.py
    Translated version from typescript to python

---
+++ src/index.py
@@ -0,0 +1,9 @@
+# Copyright(C) 2019-2023, HJD (https://github.com/hjdhjd). All rights reserved.
+# index.py: UniFi Protect API registration.
+
+from protect_api import *
+from protect_api_livestream import ProtectLivestream
+from protect_api_events import ProtectApiEvents, ProtectEventPacket
+from protect_logging import *
+from protect_types import *
+from adobe_fetch import FetchError
